6779ee672acfc8a89b9e2b8ab423b5bcb96aa524
investmentcove <investmentcove1@gmail.com>
2025-04-19 14:24:17 +0100

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -65,14 +65,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "0ca8a16c0971bce3226d8b8675128965
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "45c37ea5d116c22c1b7cb71ba0a510c2",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "22a9f639271ff55c0658ae3b5097242d",
+"flutter_bootstrap.js": "90ac82dd293bdeb97b6b0d39bceafaf6",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "1fa34b1448db63a6ae7dbabd103d82ec",
 "/": "1fa34b1448db63a6ae7dbabd103d82ec",
-"main.dart.js": "202a97fcd50da33545488b6a39f5b874",
+"main.dart.js": "aa0649a7d7b1c4e571cbfa19e397f08e",
 "manifest.json": "9bc7a1b7f0d8ddaf9fd92c67ee6d8837",
 "version.json": "81960aeb54a7e44318b588be9bd5e341"};
 // The application shell files that are downloaded before a service worker can
