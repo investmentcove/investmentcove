b091bdc24f508adbb97706419a17697ba32a3081
investmentcove <investmentcove1@gmail.com>
2025-04-20 12:36:23 +0100

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3218580006"
+    serviceWorkerVersion: "3356693496"
   }
 });
