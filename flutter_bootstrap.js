819bb73b255ba120a6b52f9a836751d2089cdaa0
investmentcove <investmentcove1@gmail.com>
2026-07-03 06:26:34 +0100

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3572159394" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "286695839" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
